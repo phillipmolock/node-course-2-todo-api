1240727e01aa08eb09ee6a7025d09444bf8e52c0
phillipmolock <phillip.molock@outlook.com>
2018-03-01 06:32:58 -0600

    add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,8 +6,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+// dummy array to fill todos after clearing
+const todos = [{
+    text: 'first test todo'
+},{
+    text: 'second test todo'
+}];
+
 beforeEach((done) => {
-   Todo.remove({}).then(() => done()); 
+   Todo.remove({}).then(() => {
+       return Todo.insertMany(todos);
+   }).then(() => done()); 
 });
 
 describe('POST /todos', () => {
@@ -25,7 +34,7 @@ describe('POST /todos', () => {
                 return done(err);
             }
             
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -45,10 +54,25 @@ describe('POST /todos', () => {
             }
             
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e) => done(e));
         });        
     });
     
 });
+
+// confirm you're getting 2 elements in todos array which are the 2 we entered 
+describe('GET /todos',() => {
+    it('should get all todos', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=> {
+            expect(res.body.todos.length).toBe(2);
+        }).end(done);
+    
+        });
+
+
+    });
