952bfccacb8dac0d645508bdc09ae2dd15e09c99
phillipmolock <phillip.molock@outlook.com>
2018-03-01 18:09:24 -0600

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -14,7 +14,9 @@ const todos = [{
     text: 'first test todo'
 },{
     _id: new ObjectID(),
-    text: 'second test todo'
+    text: 'second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -151,4 +153,64 @@ describe('DELETE /todos/:id', () => {
         .end(done); 
     });
     
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        // grab id of first item
+        
+        var id = todos[0]._id.toHexString();
+        var text = 'This should be the new text';
+        
+        // update text, set completed true
+        request(app)
+        .patch(`/todos/${id}`)
+        .send({
+            text,
+            completed: true
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(true);
+            expect(res.body.todo.completedAt).toBeA('number');
+        })
+        .end(done);
+        // assertion: status = 200
+        // assertion: response body text is changed
+        // assertion: completedAt is a number .toBeANumber
+        
+        
+    });
+    
+    it('should clear compeltedAt when todo is not completed', (done) => {
+        // grab id of second item
+        var id = todos[1]._id.toHexString();
+        var text = 'This should be the new text!!';
+        
+        // update text, set completed true
+        request(app)
+        .patch(`/todos/${id}`)
+        .send({
+            text,
+            completed: false
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(false);
+            expect(res.body.todo.completedAt).toNotExist();
+        })
+        .end(done);
+        
+        // set completed to false
+        
+        //assertion: status = 200
+        //assertion: response body text is changed
+        //assertion: completed is now false
+        //assertion: completedAt is now null .toNotExist()
+        
+        
+    });
+        
 });
