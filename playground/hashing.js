75dbfba24f6fb94daae78381331f486a6c995f3f
phillipmolock <phillip.molock@outlook.com>
2018-03-03 08:40:01 -0600

    Hash user password before save to mongodb

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,32 @@
 //const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10  
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123'); //header.payload.hash = how token is setup
-//jwt.verify
+//bcrypt.genSalt(10, (err, salt) => {
+//    bcrypt.hash(password, salt, (err, hash) => {
+//        console.log(hash);
+//    }); // .hash(password, salt, callback)
+//}); //.genSalt (rounds, callback)
 
-console.log('token', token);
+var hashedPassword = '$2a$10$WmmLTrajlXwiZ5y7D17Vqeb0kB.SgVTaAD0X1qAQp6RaIuNHA9XMm';
 
-var decoded = jwt.verify(token, '123');
-console.log('decoded', decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+   console.log(res); 
+});
+
+//var data = {
+//  id: 10  
+//};
+//
+//var token = jwt.sign(data, '123'); //header.payload.hash = how token is setup
+////jwt.verify
+//
+//console.log('token', token);
+//
+//var decoded = jwt.verify(token, '123');
+//console.log('decoded', decoded);
 
 
 
