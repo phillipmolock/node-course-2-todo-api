4eca8eff64e97413d82da72f22f570e6c800b8b0
phillipmolock <phillip.molock@outlook.com>
2018-03-04 07:20:14 -0600

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -76,6 +76,31 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    // find email first then compare hashed/salted password
+    
+    var User = this;
+    
+    return User.findOne({email}).then((user)=> {
+        if(!user){
+            return Promise.reject();
+        }
+        
+        return new Promise((resolve, reject) => {
+            // Use bcrypt.compare to compare password and user.password
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res){
+                    resolve(user);
+                } else {
+                    console.log('wrong pass, reported to NSA');
+                    reject();
+                }
+            })
+            
+        });
+    });
+};
+
 UserSchema.pre('save',function (next) {
     var user = this;
     
