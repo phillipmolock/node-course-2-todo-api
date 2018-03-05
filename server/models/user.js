1a0f45f38c297e65317d519c7255a331daf85b7d
phillipmolock <phillip.molock@outlook.com>
2018-03-04 19:01:13 -0600

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -56,6 +56,17 @@ UserSchema.methods.generateAuthToken = function () {
 
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+    return user.update({
+        $pull: {
+            tokens: {
+                token
+            }
+        }
+    });
+};
+
 // statics = model methods instead of instance, instance get called with actual document as this binding, model get called with the model as the this binding
 
 UserSchema.statics.findByToken = function (token) {
